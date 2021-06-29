cd4f7bdc16b2eb915c6516c4bff967f8a5018610
t0kar <81991067+t0kar@users.noreply.github.com>
2021-06-29 17:18:33 +0200

    minor updates - changing positions

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-//var countDownDate = new Date(2021, 09, 19, 21, 10, 00).getTime();
-var countDownDate = new Date(2021, 05, 29, 17, 09, 00).getTime();
+var countDownDate = new Date(2021, 09, 19, 21, 10, 00).getTime();
 
 var days = document.querySelector("[days]");
 var hours = document.querySelector("[hours]");
@@ -11,11 +10,6 @@ var interval = setInterval(function () {
     var now = new Date().getTime();
     var distance = countDownDate - now;
 
-    if (distance < 0) {
-        clearInterval(interval);
-        document.querySelector(".end").innerHTML = "Edukacija za Frontend developera je završila! <br> Čestitamo!";
-    }
-
     var d = Math.floor(distance / (1000 * 60 * 60 * 24));
     var h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -26,6 +20,10 @@ var interval = setInterval(function () {
     minutes.innerHTML = m < 10 ? "0" + m : m;
     seconds.innerHTML = s < 10 ? "0" + s : s;
 
+    if (distance < 0) {
+        clearInterval(interval);
+        document.querySelector(".end").innerHTML = "Edukacija za Frontend developera je završila! <br> Čestitamo!";
+    }
 
 }, 1000);
 
